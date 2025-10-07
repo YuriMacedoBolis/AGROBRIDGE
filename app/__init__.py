793a32484f4fb1d612c54e677536e1f228c890fc
Yuri Macedo <yuribolis2203@gmail.com>
2025-10-07 13:41:42 -0300

    arrumando requirements

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+from flask_wtf.csrf import CSRFProtect
 from authlib.integrations.flask_client import OAuth
 from dotenv import load_dotenv
 import os
@@ -22,6 +23,8 @@ migrate = Migrate(app ,db)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+
+csrf = CSRFProtect(app)
 
 oauth = OAuth(app)
 google = oauth.register(
--- app/routes.py
+++ app/routes.py
@@ -12,7 +12,7 @@ def homepage():
 @app.route('/login', methods=["GET" , "POST"])
 def login():
     form = LoginForm()
-    if form.validate_on_submit:
+    if form.validate_on_submit():
         user = User.query.filter_by(nome = form.nome.data).first()
         if user and bcrypt.check_password_hash(user.password_hash , form.password.data):
             login_user(user)
