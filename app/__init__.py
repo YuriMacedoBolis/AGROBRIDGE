2f6324024201e5b4a3e6e2065a0d2bed3a871b72
Yuri Macedo <yuribolis2203@gmail.com>
2025-10-08 16:08:17 -0300

    quase finalizando prototipo do site

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_wtf.csrf import CSRFProtect
-from authlib.integrations.flask_client import OAuth
 from dotenv import load_dotenv
 import os
 
@@ -25,19 +24,5 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
 csrf = CSRFProtect(app)
-
-oauth = OAuth(app)
-google = oauth.register(
-    name='google',
-    client_id=os.getenv("CLIENT_ID"),
-    client_secret=os.getenv("CLIENT_SECRET"),
-    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
-    access_token_url='https://oauth2.googleapis.com/token',
-    authorize_url='https://accounts.google.com/o/oauth2/v2/auth',
-    api_base_url='https://www.googleapis.com/oauth2/v2/',
-    client_kwargs={'scope': 'openid email profile'}
-)
-
-
 
 from app import routes , models
