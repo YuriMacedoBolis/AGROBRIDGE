81a38a09a97374720514477d904b90fb58de4569
Yuri Macedo <yuribolis2203@gmail.com>
2025-10-13 09:54:53 -0300

    arrumando .env

--- app/__init__.py
+++ app/__init__.py
@@ -14,9 +14,8 @@ app = Flask(__name__)
 #CONFIGURAÇÕES DO APP
 
 
-db_url = os.environ.get('DATABASE_URL')
-app.config["SECRET_KEY"] = db_url
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
